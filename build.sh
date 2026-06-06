4b114dafcc291223339b51cbab68c8809888acf2
Edypain <mandalasiedmond@gmail.com>
2026-06-06 20:26:10 +0200

    remove unsupported --no-input from tailwind build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Build tailwind using django-tailwind + pytailwindcss
-python manage.py tailwind build --no-input
+python manage.py tailwind build
 
 # Collect static files
 python manage.py collectstatic --no-input
